5c609117d1bfb33066b337713c26f052ba7e0216
as247 <as247@vui360.com>
2020-03-09 17:03:52 +0700

    Docker/app

--- app/install.sh
+++ app/install.sh
@@ -2,6 +2,7 @@
 
 mkdir /wp
 cp ./docker-compose-nginx-proxy.yml /wp/docker-compose.yml
+cp ./docker-compose-template.yml /wp/docker-compose-template.yml
 cp wp_create.php /wp/wp_create
 chmod +x wp_create
 docker network create nginx-proxy
